6290ca1208b668622c542b1d2e366a94ae5bd44a
w5wc <w5wc@arrl.net>
2017-03-26 20:28:21 -0500

    Add Files

---
+++ bin/Debug/initozy11.bat
@@ -0,0 +1,25 @@
+loadFW 0xfffe 0x0007 ozyfw-sdr1k.hex
+msecsleep 3000
+rem Mercury v3.1 Penelope v1.6
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v21.rbf
+
+rem Mercury v3.2 Penelope v1.7
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v22.rbf
+
+rem Mercury v3.3 Penelope v1.7
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v23.rbf
+
+rem Mercury v3.3 Penelope v1.7
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v24.rbf
+
+rem Mercury v3.4 Penelope v1.8
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v25.rbf
+
+rem Mercury v3.4 Penelope v1.8
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v26.rbf
+
+rem Mercury v3.4 Penelope v1.8
+rem loadFPGA 0xfffe 0x0007 Ozy_Janus.v27.rbf
+
+rem Mercury v3.4 Penelope v1.8
+loadFPGA 0xfffe 0x0007 Ozy_Janus.v28.rbf
